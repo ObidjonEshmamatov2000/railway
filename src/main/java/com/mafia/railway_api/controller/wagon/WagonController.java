6c65e5572495ae3a385909d6db6eecf73131b539
Arslonboy <arslonnurobod99@gmail.com>
2022-02-22 21:14:47 +0500

    first commit

--- src/main/java/com/mafia/railway_api/controller/wagon/WagonController.java
+++ src/main/java/com/mafia/railway_api/controller/wagon/WagonController.java
@@ -1,14 +1,49 @@
 package com.mafia.railway_api.controller.wagon;
 
 
+import com.mafia.railway_api.model.receive.WagonReceiveDTO;
 import com.mafia.railway_api.service.WagonService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/wagon")
 public class WagonController {
     private final WagonService wagonService;
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addTrain(
+            @RequestBody WagonReceiveDTO wagonReceiveDTO
+    ) {
+        return ResponseEntity.ok(wagonService.addWagon(wagonReceiveDTO));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAllTrains() {
+        return ResponseEntity.ok(wagonService.getAllTrains());
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getById(
+            @RequestParam long id
+    ) {
+        return ResponseEntity.ok(wagonService.getById(id));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> update(
+            @RequestBody WagonReceiveDTO wagonReceiveDTO
+    ) {
+        return ResponseEntity.ok(wagonService.update(wagonReceiveDTO));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> delete(
+            @RequestParam long id
+    ) {
+        return ResponseEntity.ok(wagonService.deleteById(id));
+    }
+
 }
--- src/main/java/com/mafia/railway_api/service/TrainService.java
+++ src/main/java/com/mafia/railway_api/service/TrainService.java
@@ -1,9 +1,17 @@
 package com.mafia.railway_api.service;
 
+import com.mafia.railway_api.entity.railway.RailwayEntity;
+import com.mafia.railway_api.entity.train.TrainEntity;
+import com.mafia.railway_api.entity.train.TrainStatus;
+import com.mafia.railway_api.entity.wagon.WagonEntity;
+import com.mafia.railway_api.model.receive.TrainReceiveDTO;
 import com.mafia.railway_api.repository.TrainRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +19,48 @@ public class TrainService {
     private final ModelMapper modelMapper;
     private final TrainRepository trainRepository;
 
+
+    public TrainEntity addTrain(TrainReceiveDTO trainReceiveDTO){
+        TrainEntity trainEntity = new TrainEntity();
+        trainEntity.setRailwayEntity(new RailwayEntity()); // getRailwayEntity bolishi kerak aslida bu yerda
+        trainEntity.setTrainStatus(TrainStatus.valueOf(trainReceiveDTO.getTrainStatus()));
+        trainEntity.setName(trainReceiveDTO.getName());
+        trainEntity.setSpeed(trainReceiveDTO.getSpeed());
+        trainEntity.setStartTime(trainReceiveDTO.getStartTime());
+        trainEntity.setUpdatedBy(null);
+        trainEntity.setCreatedBy(null);
+        trainEntity.setUpdatedDate(null);
+        trainRepository.save(trainEntity);
+        return trainEntity;
+    }
+
+    public List<TrainEntity> getAllTrains(){
+        return trainRepository.findAll();
+    }
+
+    public TrainEntity getById(long id) {
+        return trainRepository.getById(id);
+    }
+
+    public TrainEntity update(TrainReceiveDTO trainReceiveDTO) {
+        TrainEntity trainEntity = new TrainEntity();
+        trainEntity.setRailwayEntity(new RailwayEntity()); // getRailwayEntity bolishi kerak aslida bu yerda----------------
+        trainEntity.setTrainStatus(TrainStatus.valueOf(trainReceiveDTO.getTrainStatus()));
+        trainEntity.setName(trainReceiveDTO.getName());
+        trainEntity.setSpeed(trainReceiveDTO.getSpeed());
+        trainEntity.setStartTime(trainReceiveDTO.getStartTime());
+        trainEntity.setUpdatedDate(new Date());
+        trainEntity.setUpdatedBy(null);
+        trainEntity.setCreatedBy(null);
+
+        return trainRepository.save(trainEntity);
+
+    }
+
+    public TrainEntity deleteById(long id) {
+        TrainEntity trainEntity = getById(id);
+        trainRepository.delete(trainEntity);
+        return trainEntity;
+    }
 
 }
